f2bb16dec893c5e07b8d7d7be56288a573349e90
Ana Julia Mattos <anajuliamattos02@gmail.com>
2025-10-24 19:45:08 -0300

    :wrench: config: Configurando ambiente

---
+++ Camila-Reis-Semi-Joias.API/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace Camila_Reis_Semi_Joias.API;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
